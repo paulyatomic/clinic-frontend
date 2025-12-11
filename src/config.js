ef26a04b662ded4211863dd03be548630ead6f97
paulyatomic <polbantilan777@gmail.com>
2025-12-12 00:52:06 +0800

    Force update API URL to Render

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 // src/config.js
-
 // const API_URL = 'http://localhost:5000/api'; 
-const API_URL = 'https://clinic-backend-rlwp.onrender.com/api'; // <--- UPDATED LINK
+const API_URL = 'https://clinic-backend-rlwp.onrender.com/api'; // <--- MUST BE UNCOMMENTED
 
 export default API_URL;

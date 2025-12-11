6055b9a68cd62804447f3b477cfc4b0f9cafde33
paulyatomic <polbantilan777@gmail.com>
2025-12-12 00:26:52 +0800

    Connected to Live Render Backend

--- src/config.js
+++ src/config.js
@@ -1,10 +1,6 @@
 // src/config.js
 
-// 1. When you are coding on your laptop, use this:
-const API_URL = 'http://localhost:5000/api';
-
-// 2. When you deploy to Render, you will COMMENT OUT the line above 
-// and UN-COMMENT the line below with your actual Render URL:
-// const API_URL = 'https://your-backend-name.onrender.com/api'; 
+// const API_URL = 'http://localhost:5000/api'; 
+const API_URL = 'https://clinic-backend-rlwp.onrender.com/api'; // <--- UPDATED LINK
 
 export default API_URL;
